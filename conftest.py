df89c526f2d0182535c3ca4972a04100bfdd3218
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-05-07 22:53:00 +0200

    update fixtures, add tests, which gonna be skipped cause of bugs ()

--- conftest.py
+++ conftest.py
@@ -1,24 +1,17 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(params=["Chrome", "Firefox", "Safari"])
+@pytest.fixture(params=["Chrome", "Firefox"], scope="function")
 def browser(request):
-    if request.param == "Chrome":
-        return request.getfixturevalue("chrome")
-    if request.param == "Firefox":
-        return request.getfixturevalue("firefox")
-    if request.param == "Safari":
-        return request.getfixturevalue("safari")
-
-
-
-@pytest.fixture(params=[(1920, 1080), (1280, 720)])
-def desktop_browser(request):
-    width, height = request.param
-    driver = webdriver.Chrome()
-    driver.set_window_size(width, height)
+    driver = webdriver.Chrome() if request.param == "Chrome" else webdriver.Firefox()
     yield driver
     driver.quit()
+
+@pytest.fixture(params=[(1920, 1080), (1280, 720)])
+def desktop_browser(request, browser):
+    width, height = request.param
+    browser.set_window_size(width, height)
+    yield browser
 
 @pytest.fixture(params=[(480, 800), (300, 500)])
 def mobile_browser(request):
@@ -36,4 +29,5 @@ def is_mobile(request):
     else:
         return True
 
-desktop_only = pytest.mark.parametrize("is_mobile", [(1920, 1080), (1280, 720)], indirect=True)
+desktop_only = pytest.mark.parametrize("desktop_browser", [(1920, 1080), (1280, 720)], indirect=True)
+chrome_only = pytest.mark.parametrize("browser", ["Chrome"], indirect=True)
--- tests/test_skip_due_to_bug.py
+++ tests/test_skip_due_to_bug.py
@@ -1,10 +1,13 @@
 import pytest
 
+from conftest import chrome_only
+
 pytestmark = pytest.mark.skip(reason="this scope of tests skipped cause of bug TASK-1234")
 
 def test_first():
     assert True
 
+@chrome_only
 def test_second(desktop_browser):
     desktop_browser.get('http://google.com')
     assert desktop_browser.title == 'Google'
@@ -14,4 +17,3 @@ def test_second(desktop_browser):
 def test_third_marked(desktop_browser):
     desktop_browser.get('http://google.com')
     assert desktop_browser.title == 'Google'
-
